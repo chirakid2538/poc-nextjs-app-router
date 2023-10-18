0d1264f12ab6f40ec94410529d9265773c2cbd34
chirakid kaweesirikon <chirakid.k@gmail.com>
2023-10-18 15:01:23 +0700

    :art: update
    update

---
+++ src/app/(auth)/auth/otp/@channel/phone/page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Page() {
+  return <div>auth/otp/channel/phone</div>;
+}
---
+++ src/app/(auth)/auth/otp/not-found.tsx
@@ -0,0 +1,9 @@
+/**
+ * if not found path in nested
+ */
+
+const NotFound = () => {
+  return <div>auth/otp:not-found</div>;
+};
+
+export default NotFound;
